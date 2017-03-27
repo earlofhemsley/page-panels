d92c5a5c37532e3449db4ee1f25ad09bc0d703d6
Landon Hemsley <lhemsley@egovmt.com>
2017-03-27 07:40:39 -0600

    initial settings layout for plugin added

--- experience-panels.php
+++ experience-panels.php
@@ -16,7 +16,53 @@ function experience_panel_registration(){
 }
 
 function experience_panel_html(){
-    echo "Hello World";
+    
+    //number of tiles to put in the panel
+    //number of rows in the panel
+    //number of columns in mobile (xs)
+    //number of columns on desktop (sm and up)
+
+    $numTilesOptions = "";
+    $numRowsOptions = "";
+    $numColumnsMobile = "";
+    $numColumnsDesktop = "";
+
+    for($i=1; $i<=15; $i++){
+        $numTilesOptions .= "<option value='$i'>$i</option>";
+        if($i<=5) $numRowsOptions .= "<option value='$i'>$i</option>";
+        if($i<=12){ 
+            $numColumnsMobile .= "<option value='$i'>$i</option>";
+            $numColumnsDesktop .= "<option value='$i'>$i</option>";
+        }
+    }
+
+    $plugin_options = array(
+        'numTiles' => array("label" => "Number of tiles in panel", "options" => $numTilesOptions), 
+        'numRows' => array("label" => "Number of rows in the panel", "options" => $numRowsOptions),
+        'numColumnsMobile' => array("label" => "Number of columns in mobile view", "options" => $numColumnsMobile), 
+        'numColumnsDesktop' => array("label" => "Number of columns in desktop view", "options" => $numColumnsDesktop),
+    );
+?>
+    <h2>Experience Panel Plugin Settings</h2>
+    <p>Please input the required options for the Experience Panel.</p>
+    <form method="POST">
+        <div>
+            <dl>
+<?php
+                foreach($plugin_options as $key => $option){
+                    echo "<dt>".$option["label"]."</dt>";
+                    echo "<dd><select name='$key'>".$option["options"]."</select></dd>";
+                } 
+?>
+            </dl>
+        </div>
+        <button type="submit">Save Settings</button>
+    </form>
+
+<?php
+    
+    //TODO: Import new stylesheets as part of the plugin
+
 }
 
 
