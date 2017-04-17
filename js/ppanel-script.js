5911b1ae8b9a4846b3ac1feaa442430ab2998509
Landon Hemsley <lhemsley@egovmt.com>
2017-04-17 06:32:59 -0600

    save uncommitted change from previously

--- js/ppanel-script.js
+++ js/ppanel-script.js
@@ -2,8 +2,8 @@
 
 if(jQuery){
     var frameTiles = function(){
-        var height = 100;
-        $('.ppanel-container li').delay(1000).each(function(){
+        var height = 0;
+        $('.ppanel-container li').each(function(){
             height = $(this).height() > height ? $(this).height() : height;
         });
         $('.ppanel-container li').css('height', height);
