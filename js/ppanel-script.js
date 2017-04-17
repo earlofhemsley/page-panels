46973390e3f7ac18e682b5cd64c91258a2e7516d
Landon Hemsley <lhemsley@egovmt.com>
2017-04-17 07:25:38 -0600

    adjust front-end to make responsive

--- js/ppanel-script.js
+++ js/ppanel-script.js
@@ -2,11 +2,16 @@
 if(jQuery){
     var frameTiles = function(){
         var height = 0;
-        $('.ppanel-container .ppanel-tile-image').each(function(){
+        $('.ppanel-container .ppanel-tile-image img').each(function(){
             height = $(this).height() + 8 > height ? $(this).height() + 8 : height;
         });
         $('.ppanel-container .ppanel-tile-image').css('height', height);
     };
 
     $(window).load(frameTiles);
+
+    $(window).on('resize', function(){
+        frameTiles();
+    })
+
 }
