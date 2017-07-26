77e6ca4010422ccf6f59dbfec7f911b9761b538e
Landon Hemsley <lhemsley@egovmt.com>
2017-07-26 06:57:08 -0600

    jquery compatibility issues resolved

--- js/ppanel-script.js
+++ js/ppanel-script.js
@@ -1,29 +1,30 @@
 /* Custom JS for ppanels */
 if(jQuery){
-    var frameTiles = function(){
-        var height = 0;
-        $('.ppanel-container .ppanel-tile-image img').each(function(){
-            height = $(this).height() + 6 > height ? $(this).height() + 6 : height;
+    jQuery(document).ready(function($){
+        var frameTiles = function(){
+            var height = 0;
+            $('.ppanel-container .ppanel-tile-image img').each(function(){
+                height = $(this).height() + 6 > height ? $(this).height() + 6 : height;
+            });
+            $('.ppanel-container .ppanel-tile-image').css('height', height);
+        };
+
+        $(window).load(frameTiles);
+
+        $(window).on('resize', function(){
+            frameTiles();
         });
-        $('.ppanel-container .ppanel-tile-image').css('height', height);
-    };
 
-    $(window).load(frameTiles);
+        $('img.modalable').on('click', function(){
+            $('body').addClass('ppanel-modal-open');
+            $('.ppanel-modal-backdrop').css('display','block');
+            $($(this).data('show')).fadeIn();
+        });
 
-    $(window).on('resize', function(){
-        frameTiles();
+        $('span.ppanel-xish').on('click', function(){
+            $('body').removeClass('ppanel-modal-open');
+            $('.ppanel-modal-backdrop').css('display','none');
+            $('.ppanel-modal-wrapper').fadeOut();
+        });
     });
-
-    $('img.modalable').on('click', function(){
-        $('body').addClass('ppanel-modal-open');
-        $('.ppanel-modal-backdrop').css('display','block');
-        $($(this).data('show')).fadeIn();
-    });
-
-    $('span.ppanel-xish').on('click', function(){
-        $('body').removeClass('ppanel-modal-open');
-        $('.ppanel-modal-backdrop').css('display','none');
-        $('.ppanel-modal-wrapper').fadeOut();
-    });
-
 }
